0d97c89fad4c51e54db87a29f6df99c5b5dba570
lamees97 <lamees-1997@hotmail.com>
2020-01-20 09:31:22 +0300

    add one space

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -29,7 +29,7 @@ export default function HomeScreen() {
       querySnapshot.forEach(doc => {
         messages.push({ id: doc.id, ...doc.data() });
       });
-      console.log("Current messages: ", messages);
+      console.log(" Current messages: ", messages);
       setMessages([...messages]);
     });
   }, []);
